089865d0a18a10695c461312d0dce8a83a293386
arcessebas <sebastian.arce12@inacapmail.cl>
2025-12-11 11:24:37 -0300

    Add files via upload

---
+++ electron/main.js
@@ -0,0 +1,23 @@
+import { app, BrowserWindow } from "electron";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const isDev = !app.isPackaged;
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js")
+    }
+  });
+  win.webContents.openDevTools(); // ← Agrega esto aquí
+  if (isDev) {
+    win.loadURL("http://localhost:5173");
+  } else {
+    win.loadFile(path.join(__dirname, "../dist/index.html"));
+  }
+}
